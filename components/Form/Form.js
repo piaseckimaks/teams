3c7f38615992799dd4d8527c087457792a3a594b
Maksym Piasecki <piaseckimaks94@gmail.com>
2021-12-31 06:04:34 +0100

    signin signup responsive

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -8,16 +8,15 @@ export default function Form({ handleRegistration, handleLogin, isSignForm }) {
 
 
     return (
-      <div className="hero h-full w-full mx-auto bg-fixed" style={{backgroundImage: 'url("https://images.pexels.com/photos/1527934/pexels-photo-1527934.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")'}} >
+      <div className=" h-full w-full mx-auto relative bg-fixed" style={{backgroundImage: 'url("https://images.pexels.com/photos/1527934/pexels-photo-1527934.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")'}} >
           <div className="hero-overlay bg-opacity-40"></div> 
-          <div className="text-center hero-content text-neutral-content">
+          <div className="absolute top-0 left-0 py-2 text-neutral-content h-full w-full flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center  overflow-auto">
             <Link href="/">
-              <a>
+              <a className=''>
                 <Logo size="9xl" />
               </a>
             </Link>
-            <div className="divider divider-vertical"></div>
-
+            <div className="border w-11/12 my-2 md:h-3/4 md:mx-5 md:w-0"></div>
             {isSignForm ? <SignIn handleLogin={handleLogin} /> : <SignUp handleRegistration={handleRegistration}/>}
 
           </div>

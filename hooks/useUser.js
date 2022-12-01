35dba9ca3f0321e3612c8ef2ffcf4702fc2ade78
piaseckimaks <69255092+piaseckimaks@users.noreply.github.com>
2022-12-01 12:29:08 +0100

    Update useUser.js

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -11,7 +11,7 @@ const fetcher = (url) =>
 
 export function useUser({ redirectTo, redirectIfFound } = {}) {
   const { data, error } = useSWR('/api/auth/user', fetcher)
-  const user = data?.user
+  let user = data?.user
   const finished = Boolean(data)
   const hasUser = Boolean(user)
 
